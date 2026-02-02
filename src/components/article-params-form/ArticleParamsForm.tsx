f4080623d9013d8e2e72454fd646ed0eb0c26edc
dandreev <Azazello1992Kot!>
2026-02-02 14:44:56 +0300

    реализовал работу aside

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,15 +2,137 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import React, { FormEvent, useEffect, useRef, useState } from 'react';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	handleFontFamilyChange: (select: OptionType) => void;
+	handleFontSizeChange: (select: OptionType) => void;
+	handleFontColorChange: (select: OptionType) => void;
+	handleBackgroundColorChange: (select: OptionType) => void;
+	handleContentWidthChange: (select: OptionType) => void;
+	applySideBarState: (event: FormEvent) => void;
+	resetSideBarState: () => void;
+};
+
+type TUseHandleClickOutsideOptions = {
+	isOpen: boolean;
+	asideRef: React.RefObject<HTMLElement>;
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function shouldCloseAside(
+	e: MouseEvent,
+	asideRef: React.RefObject<HTMLElement>,
+	isOpen: boolean
+): boolean {
+	return (
+		isOpen && !!asideRef.current && !asideRef.current.contains(e.target as Node)
+	);
+}
+
+const useHandleClickOutside = (prop: TUseHandleClickOutsideOptions) => {
+	const { isOpen, asideRef, setOpen } = prop;
+
+	useEffect(() => {
+		const handleClick = (e: MouseEvent) => {
+			if (shouldCloseAside(e, asideRef, isOpen)) {
+				setOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClick);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClick);
+		};
+	}, [isOpen]);
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	handleFontFamilyChange,
+	handleFontSizeChange,
+	handleFontColorChange,
+	handleBackgroundColorChange,
+	handleContentWidthChange,
+	applySideBarState,
+	resetSideBarState,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setOpen] = useState(false);
+	const ref = useRef<HTMLFormElement | null>(null);
+
+	useHandleClickOutside({ isOpen, asideRef: ref, setOpen });
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setOpen(!isOpen);
+				}}
+			/>
+			<aside
+				ref={ref}
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
+				<form className={styles.form} onSubmit={applySideBarState}>
+					<Text as={'h1'} size={31} weight={800} uppercase={true}>
+						задайте параметры
+					</Text>
+					<Select
+						selected={articleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='шрифт'
+						onChange={handleFontFamilyChange}
+					/>
+					<RadioGroup
+						name={'fontSize'}
+						options={fontSizeOptions}
+						selected={articleState.fontSizeOption}
+						title='размер шрифта'
+						onChange={handleFontSizeChange}
+					/>
+					<Select
+						selected={articleState.fontColor}
+						options={fontColors}
+						title='цвет шрифта'
+						onChange={handleFontColorChange}
+					/>
+					<Separator />
+					<Select
+						selected={articleState.backgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+						onChange={handleBackgroundColorChange}
+					/>
+					<Select
+						selected={articleState.contentWidth}
+						options={contentWidthArr}
+						title='ширина контента'
+						onChange={handleContentWidthChange}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={resetSideBarState}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, FormEvent } from 'react';
 import clsx from 'clsx';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { Article } from 'components/article';
+import { ArticleParamsForm } from 'components/article-params-form';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	OptionType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +17,62 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [sideBarState, setSideBarState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const [articleState, setArticleState] = useState(defaultArticleState);
+
+	const handleFontFamilyChange = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, fontFamilyOption: select });
+	};
+
+	const handleFontSizeChange = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, fontSizeOption: select });
+	};
+
+	const handleFontColorChange = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, fontColor: select });
+	};
+
+	const handleBackgroundColorChange = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, backgroundColor: select });
+	};
+
+	const handleContentWidthChange = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, contentWidth: select });
+	};
+
+	const applySideBarState = (event: FormEvent) => {
+		event.preventDefault();
+		setArticleState({ ...sideBarState });
+	};
+
+	const resetSideBarState = () => {
+		setArticleState(defaultArticleState);
+		setSideBarState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				articleState={sideBarState}
+				handleFontFamilyChange={handleFontFamilyChange}
+				handleFontSizeChange={handleFontSizeChange}
+				handleFontColorChange={handleFontColorChange}
+				handleBackgroundColorChange={handleBackgroundColorChange}
+				handleContentWidthChange={handleContentWidthChange}
+				applySideBarState={applySideBarState}
+				resetSideBarState={resetSideBarState}
+			/>
 			<Article />
 		</main>
 	);
